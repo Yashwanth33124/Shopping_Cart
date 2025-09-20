8b48029644b05229f797b57110848b30ffbe573f
Yashwanth <cheruvuyashwanth99@gmail.com>
2025-09-20 22:42:50 +0530

    Add default route redirect to product-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React, { Fragment } from 'react';
 import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProductList from './pages/productList'; 
 import ProductDetails from './pages/productdetails';
 import CartList from './pages/cartlist';
 
-
 function App() {
 
   return (
-  <Fragment>
-    <Routes>
-      <Route path="/product-list" element={<ProductList/>}/>
-      <Route path='/product-details/:id' element={<ProductDetails/>}/>
-      <Route path='/cart' element={<CartList/>}/>
-    </Routes>
-  </Fragment>
+    <Fragment>
+      <Routes>
+        {/* Redirect root "/" to "/product-list" */}
+        <Route path="/" element={<Navigate to="/product-list" />} />
+
+        <Route path="/product-list" element={<ProductList/>}/>
+        <Route path="/product-details/:id" element={<ProductDetails/>}/>
+        <Route path="/cart" element={<CartList/>}/>
+      </Routes>
+    </Fragment>
   )
 }
 
-export default App
+export default App;
